39c39a6e18e2f9481944e23affc96a40ac7c29e6
Cedarku <286011590+Cedarku@users.noreply.github.com>
2026-07-09 12:30:04 +0200

    Complete exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter((value) => !remove.includes(value))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
