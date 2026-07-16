a3f4754025f56620f178328c06509d255e0fb8ad
Cedarku <286011590+Cedarku@users.noreply.github.com>
2026-07-16 12:36:49 +0200

    Make game fully functional with buttons

--- script.js
+++ script.js
@@ -9,29 +9,59 @@ function getComputerChoice() {
     }
 }
 
-const btnRocks = document.querySelector(".btnRocks");
-btnRocks.addEventListener("click", function() {
+let playerScore = 0;
+let computerScore = 0;
+
+function incrementScore(roundResult) {
+    const winMessage = "You win!";
+    const loseMessage = "You lose!";
+    
+    if (roundResult === winMessage) {
+        playerScore += 1;
+    } else if (roundResult === loseMessage) {
+        computerScore += 1;
+    }}
+
+const btnRock = document.querySelector(".btnRock");
+btnRock.addEventListener("click", function() {
     const computerChoice = getComputerChoice();
     const playerSelection = "Rock";
-    playRound(playerSelection, computerChoice)});
+    console.log("Computer: " + computerChoice);
+    console.log("You: " + playerSelection);
+    playRound(playerSelection, computerChoice);
+    const roundResult = playRound(playerSelection, computerChoice);
+    incrementScore(roundResult);
+    console.log(roundResult);
+    console.log("You: " + playerScore + " Computer: " + computerScore);}
+);
 
 const btnPaper = document.querySelector(".btnPaper");
 btnPaper.addEventListener("click", function() {
     const computerChoice = getComputerChoice();
     const playerSelection = "Paper";
-    playRound(playerSelection, computerChoice)});
+    console.log("Computer: " + computerChoice);
+    console.log("You: " + playerSelection);
+    playRound(playerSelection, computerChoice);
+    const roundResult = playRound(playerSelection, computerChoice);
+    incrementScore(roundResult);
+    console.log(roundResult);
+    console.log("You: " + playerScore + " Computer: " + computerScore);}
+);
 
 const btnScissors = document.querySelector(".btnScissors");
 btnScissors.addEventListener("click", function() {
     const computerChoice = getComputerChoice();
     const playerSelection = "Scissors";
-    playRound(playerSelection, computerChoice)});
+    console.log("Computer: " + computerChoice);
+    console.log("You: " + playerSelection);
+    playRound(playerSelection, computerChoice);
+    const roundResult = playRound(playerSelection, computerChoice);
+    incrementScore(roundResult);
+    console.log(roundResult);
+    console.log("You: " + playerScore + " Computer: " + computerScore);}
+);
 
-function playGame () {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    function playRound(playerSelection, computerChoice) {
+function playRound(playerSelection, computerChoice) {
     if (
         (playerSelection === "Rock" && computerChoice === "Scissors") ||
         (playerSelection === "Paper" && computerChoice === "Rock") ||
@@ -45,6 +75,11 @@ function playGame () {
     else { return "You lose!"; }
     }
 
+/*
+function playGame () {
+    let playerScore = 0;
+    let computerScore = 0;
+
     function incrementScore(roundResult) {
     const winMessage = "You win!";
     const loseMessage = "You lose!";
@@ -55,7 +90,7 @@ function playGame () {
         computerScore += 1;
     }}
 
-    /*for (let round = 0; round < 5; round++) {
+    for (let round = 0; round < 5; round++) {
         const computerChoice = getComputerChoice();
         const playerSelection = getplayerSelection();
         console.log(computerChoice);
@@ -67,7 +102,7 @@ function playGame () {
         incrementScore(roundResult);
         console.log(roundResult);
         console.log("You: " + playerScore + " Computer: " + computerScore);
-    } */
+    } 
 }
 
 function finalScore(playerScore, computerScore) {
@@ -83,6 +118,7 @@ function finalScore(playerScore, computerScore) {
     
 playGame();
 console.log(finalScore());
+*/
 
 
 
