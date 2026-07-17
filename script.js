6dbff4aa5d393a41b36b395ba058af1c4eddaea6
Cedarku <286011590+Cedarku@users.noreply.github.com>
2026-07-17 11:36:35 +0200

    Redesign game interface

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ let computerScore = 0;
 
 const btnRock = document.querySelector(".btnRock");
 btnRock.addEventListener("click", () => playGame("Rock"));
-
 const btnPaper = document.querySelector(".btnPaper");
 btnPaper.addEventListener("click", () => playGame("Paper"));
-
 const btnScissors = document.querySelector(".btnScissors");
 btnScissors.addEventListener("click", () => playGame("Scissors"));
 
+const finalScoreTable = document.querySelector(".finalScoreTable");
+finalScoreTable.style.display = "none";
 const btnReplay = document.querySelector(".btnReplay");
 btnReplay.style.display = "none";
 btnReplay.addEventListener("click", () => { location.reload() });
-
 
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
@@ -78,55 +77,23 @@ function updateDisplay(playerChoice, computerChoice, roundResult, playerScore, c
 
 function finalScore(playerScore, computerScore) {
     if (playerScore === 5) {
-        const tableDisplay = document.getElementById("display");
-        tableDisplay.style.display = "none";
         const finalMessage = document.getElementById("finalMessage");
         finalMessage.textContent = "You win!";
     } else if (computerScore === 5) {
-        const tableDisplay = document.getElementById("display");
-        tableDisplay.style.display = "none";
         const finalMessage = document.getElementById("finalMessage");
         finalMessage.textContent = "You lose!";
     } else {
         return;
     }
 
-    btnRock.disabled = true;
-    btnPaper.disabled = true;
-    btnScissors.disabled = true;
+    const tableDisplay = document.getElementById("display");
+    tableDisplay.style.display = "none";
+    const choiceMenu = document.querySelector(".choiceMenu");
+    choiceMenu.style.display = "none";
+    const finalPlayerScore = document.getElementById("finalPlayerScore");
+    const finalComputerScore = document.getElementById("finalComputerScore");
+    finalPlayerScore.textContent = playerScore;
+    finalComputerScore.textContent = computerScore;
+    finalScoreTable.style.display = "";
     btnReplay.style.display = "";
 }
-
-
-
-/*
-    for (let round = 0; round < 5; round++) {
-        const computerChoice = getComputerChoice();
-        const playerChoice = getplayerChoice();
-        console.log(computerChoice);
-        console.log(playerChoice);
-
-        playRound(playerChoice, computerChoice)
-
-        const roundResult = playRound(playerChoice, computerChoice);
-        incrementScore(roundResult);
-        console.log(roundResult);
-        console.log("You: " + playerScore + " Computer: " + computerScore);
-    } 
-}
-
-function finalScore(playerScore, computerScore) {
-    const finalMessage = "Final result: ";
-    if (playerScore > computerScore) {
-        return finalMessage + "Congratulations, you win!";
-    } else if (playerScore < computerScore) {
-        return finalMessage + "You lose.";
-    } else {
-        return finalMessage + "It's a draw.";
-    }
-}
-    
-playGame();
-console.log(finalScore());
-*/
-
