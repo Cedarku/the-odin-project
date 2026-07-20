eead08c6a6313dde46b93833e58bdbe16786189f
Cedarku <286011590+Cedarku@users.noreply.github.com>
2026-07-20 12:34:52 +0200

    Add grid and paint function

---
+++ script.js
@@ -0,0 +1,12 @@
+const grid = document.querySelector(".grid");
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
+
+    square.addEventListener("mouseenter", () => {
+    square.style.backgroundColor= "black";}
+    );
+}
+
