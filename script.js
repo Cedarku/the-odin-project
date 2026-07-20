d9cf0e5917f4c505c686073b239f3a05e1d647c8
Cedarku <286011590+Cedarku@users.noreply.github.com>
2026-07-20 13:26:39 +0200

    Add button to set grid size

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 const grid = document.querySelector(".grid");
+const btnSize = document.querySelector(".btnSize");
 
+// Button function
+btnSize.addEventListener("click", () => {
+    let size = getSize();
+    createGrid(size)}
+);
+
+// Get size
+function getSize() {
+    return prompt("Enter the number of squares per side (max 100):");
+}
+
+function createGrid(size) {
+    grid.innerHTML = "";
+    let numSquares = size * size;
+
+    for (let i = 0; i < numSquares; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        grid.appendChild(square);
+
+        square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor= "black";}
+    );
+}
+}
+
+createGrid(16);
+
+/*
+// Create the grid
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
@@ -9,4 +40,4 @@ for (let i = 0; i < 256; i++) {
     square.style.backgroundColor= "black";}
     );
 }
-
+*/
