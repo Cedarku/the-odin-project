12ee7597bd741305d58b51a41279ecc992ac0f09
Cedarku <286011590+Cedarku@users.noreply.github.com>
2026-07-21 13:48:09 +0200

    Add switch to active rainbow mode

--- script.js
+++ script.js
@@ -43,11 +43,25 @@ function createGrid(size) {
 
 function paintSquare(event) {
     const square = event.target;
-    if (RandomMode) {
+    if (rainbowMode) {
         square.style.backgroundColor = getRandomColor();
     } else {
         square.style.backgroundColor = "black";
     }
 }
+
+let rainbowMode = false;
+const rainbowSwitch = document.querySelector("#rainbowMode");
+rainbowSwitch.addEventListener("change", () => {
+    rainbowMode = rainbowSwitch.checked;
+});
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 
 createGrid(16);
