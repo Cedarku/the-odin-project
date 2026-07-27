b321c043721b8db75edfea44fc68aa37aa1c62a9
Cedarku <286011590+Cedarku@users.noreply.github.com>
2026-07-27 16:56:34 +0200

    feat: add digit button functionality and update display
    - Store clicked number digits into variable 'a'
    - Update calculator display dynamically on digit click

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
 import { operate } from "./operate.js";
+
+const display = document.querySelector(".display");
+const btnNumber = document.querySelectorAll(".btn-number");
+
+let a = "";
+function aNumber(number) {
+    if (a === "" || a === "0") {
+        a = number;
+    } else {
+        a += number;
+    }
+
+    display.textContent = a;
+}
+
+
+
+btnNumber.forEach( button => {
+    button.addEventListener("click", (event) => {
+        const getNumber = event.target.dataset.number;
+
+        aNumber(getNumber);
+    });
+});
